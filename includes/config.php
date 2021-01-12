6e6c55ff8ba70d27d5bcc26dc759dc411d28b0ed
kedahfc <77327984+kedahfc@users.noreply.github.com>
2021-01-13 02:44:38 +0800

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,12 +1,16 @@
-//Get Heroku ClearDB connection information
-$cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server   = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db       = substr($cleardb_url["heroku_fabc276d92d4c52"],1);
-
-$active_group = 'default';
-$query_builder = TRUE;
-
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+<?php 
+// DB credentials.
+define('DB_HOST','us-cdbr-east-03.cleardb.com');
+define('DB_USER','b6be6324539b6d');
+define('DB_PASS','57f7729');
+define('DB_NAME','heroku_fabc276d92d4c52');
+// Establish database connection.
+try
+{
+$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+}
+catch (PDOException $e)
+{
+exit("Error: " . $e->getMessage());
+}
+?>
