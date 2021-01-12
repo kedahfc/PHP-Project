acdf97517ba0fb8cd79f017b693ce4870cab90e9
kedahfc <77327984+kedahfc@users.noreply.github.com>
2021-01-13 02:52:37 +0800

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -5,12 +5,9 @@ define('DB_USER','b6be6324539b6d');
 define('DB_PASS','57f7729');
 define('DB_NAME','heroku_fabc276d92d4c52');
 // Establish database connection.
-try
-{
-$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-}
-catch (PDOException $e)
-{
-exit("Error: " . $e->getMessage());
-}
+$active_group = 'default';
+$query_builder = TRUE;
+
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
