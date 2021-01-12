2ef7f44682c5252e91f940c41ec5b154337e4584
kedahfc <77327984+kedahfc@users.noreply.github.com>
2021-01-12 20:22:57 +0800

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,75 @@
+
+ <!-- ======= Header ======= -->
+ <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <nav class="nav-menu d-none d-lg-block" style=margin-top:20px>
+
+        <ul>
+
+        <li class="drop-down" style=margin-right:-10px><a href="#">About</a>
+            <ul>
+              <li><a href="page.php?type=aboutus">About Us</a></li>
+              <li><a href="page.php?type=history">History</a></li>
+              <li class="drop-down"><a href="#">Club Structure</a>
+                <ul>
+                  <li><a href="senior-officials.php">Senior Officials</a></li>
+                  <li><a href="team-officials.php">Team Officials</a></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+
+          <li><a href="news.php" style=margin-right:-10px>News</a></li>
+
+          <li class="drop-down" style=margin-right:-10px><a href="#">Teams</a>
+            <ul>
+              <li><a href="current-squad.php">Current Squad</a></li>
+              <li><a href="results.php">Results</a></li>
+            </ul>
+          </li>
+
+          <li class="drop-down" style=margin-right:-10px><a href="#">Standings</a>
+            <ul>
+              <li><a href="standings.php">Standings</a></li>
+              <li><a href="honours.php">Honours</a></li>
+            </ul>
+          </li>
+
+          <li><a href="fixtures.php" style=margin-right:-5px>Fixtures</a></li>
+
+        </ul> 
+      </nav><!-- .nav-menu -->
+
+      <h1 class="logo"><a href="index.php"></a>
+      <a href="index.php" class="logo"><img src="assets/img/logo/logo center 2.png" class="img-fluid"></a>
+      </h1>
+      
+   
+
+        <a href="ticket.php" class="get-started-btn" style=margin-top:30px>Ticket</a>
+        <a href="membership.php" class="get-started-btn" style=margin-top:30px>Membership</a>
+        <a href="shop.php" class="get-started-btn" style=margin-top:30px>Shop</a>
+  
+      <nav class="nav-menu2 d-none d-lg-block" style=margin-top:20px>
+        <ul>
+          <li class="get-started-btn drop-down2" a href="">Login</a>
+            <ul>
+              <li><a href="admin-login.php">Admin</a></li>
+              <li><a href="user-login.php">User</a></li>
+            </ul>
+          </li>
+        </ul> 
+      </nav>
+
+    </div>
+
+    <div class=line></div>
+
+  </header>
+  
+  <!-- End Header -->
+
+ <!-- Template Main JS File -->
+ <script src="assets/js/main.js"></script>
+
