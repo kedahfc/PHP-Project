8c166fbe1c6d416cda07c53893078e54111b478e
kedahfc <77327984+kedahfc@users.noreply.github.com>
2021-01-12 19:08:28 +0800

    Add files via upload

---
+++ admin/includes/header.php
@@ -0,0 +1,36 @@
+<div class="wrapper">
+
+<nav class="topnav navbar navbar-light">
+  <button type="button" class="navbar-toggler text-muted mt-2 p-0 mr-3 collapseSidebar">
+    <i class="fe fe-menu navbar-toggler-icon"></i>
+  </button>
+
+  <ul class="nav">
+    <li class="nav-item">
+      <a class="nav-link text-muted my-2" href="#" id="modeSwitcher" data-mode="dark">
+        <i class="fe fe-sun fe-16"></i>
+      </a>
+    </li>
+
+
+
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle text-muted pr-0" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <span class="avatar avatar-sm mt-2">
+          <img src="./assets/img/logo/logo.png" alt="..." class="avatar-img rounded-circle">
+        </span>
+      </a>
+      <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+        <a class="dropdown-item" href="change-password.php">Change Password</a>
+        <a class="dropdown-item" href="logout.php">Log out</a>
+      </div>
+    </li>
+  </ul>
+</nav>
+
+<aside class="sidebar-left border-right bg-white shadow" id="leftSidebar" data-simplebar>
+        <a href="#" class="btn collapseSidebar toggle-btn d-lg-none text-muted ml-2 mt-3" data-toggle="toggle">
+          <i class="fe fe-x"><span class="sr-only"></span></i>
+        </a>
+        <nav class="vertnav navbar navbar-light">
+
---
+++ admin/includes/sidebar.php
@@ -0,0 +1,131 @@
+
+  <aside class="sidebar-left border-right bg-white shadow" id="leftSidebar" data-simplebar>
+        <a href="#" class="btn collapseSidebar toggle-btn d-lg-none text-muted ml-2 mt-3" data-toggle="toggle">
+          <i class="fe fe-x"><span class="sr-only"></span></i>
+        </a>
+        <nav class="vertnav navbar navbar-light">
+
+          <!-- nav bar -->
+          <div class="w-100 mb-4 d-flex">
+            <a class="navbar-brand mx-auto mt-2 flex-fill text-center" href="./dashboard.php">
+            <span class="avatar avatar-sm mt-5">
+          <img src="./assets/img/logo/logo.png" alt="..." class="avatar-img rounded-circle">
+        </span>
+                <g>
+                  <polygon class="st0" points="78,105 15,105 24,87 87,87 	" />
+                  <polygon class="st0" points="96,69 33,69 42,51 105,51 	" />
+                  <polygon class="st0" points="78,33 15,33 24,15 87,15 	" />
+                </g>
+              </svg>
+            </a>
+          </div>
+
+          <ul class="navbar-nav flex-fill w-100 mb-2">
+          <li class="nav-item w-100">
+              <a class="nav-link" href="dashboard.php">
+                <i class="fe fe-home fe-16"></i>
+                <span class="ml-3 item-text">Dashboard</span>
+              </a>
+            </li>
+
+            <li class="nav-item w-100">
+              <a class="nav-link" href="manage-pages.php">
+                <i class="fe fe-file-text fe-16"></i>
+                <span class="ml-3 item-text">Pages</span>
+              </a>
+            </li> 
+
+            <li class="nav-item w-100">
+              <a class="nav-link" href="seniorofficials.php">
+                <i class="fe fe-framer fe-16"></i>
+                <span class="ml-3 item-text">Club Stucture</span>
+              </a>
+            </li> 
+
+            <li class="nav-item w-100">
+              <a class="nav-link" href="manage-news.php">
+                <i class="fe fe-book-open fe-16"></i>
+                <span class="ml-3 item-text">News</span>
+              </a>
+            </li> 
+
+            <li class="nav-item dropdown">
+              <a href="#users" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
+                <i class="fe fe-users fe-16"></i>
+                <span class="ml-3 item-text">Team</span>
+              </a>
+              <ul class="collapse list-unstyled pl-4 w-100" id="users">
+                <a class="nav-link pl-3" href="manage-player.php"><span class="ml-1">Current Squad</span></a>
+                <a class="nav-link pl-3" href="results.php"><span class="ml-1">Results</span></a>
+              </ul>
+            </li>
+            
+            <li class="nav-item w-100">
+              <a class="nav-link" href="standings.php">
+                <i class="fe fe-award fe-16"></i>
+                <span class="ml-3 item-text">Standings</span>
+              </a>
+            </li> 
+
+
+            <li class="nav-item w-100">
+              <a class="nav-link" href="matches.php">
+                <i class="fe fe-life-buoy fe-16"></i>
+                <span class="ml-3 item-text">Fixtures</span>
+              </a>
+            </li> 
+
+            <li class="nav-item w-100">
+              <a class="nav-link" href="notice-agenda.php">
+                <i class="fe fe-git-merge fe-16"></i>
+                <span class="ml-3 item-text">Meeting</span>
+              </a>
+            </li> 
+
+            <li class="nav-item w-100">
+              <a class="nav-link" href="membership.php">
+                <i class="fe fe-slack fe-16"></i>
+                <span class="ml-3 item-text">Membership Plan</span>
+              </a>
+            </li> 
+    
+
+            <li class="nav-item w-100">
+              <a class="nav-link" href="manage-product.php">
+                <i class="fe fe-shopping-bag fe-16"></i>
+                <span class="ml-3 item-text">Products</span>
+              </a>
+            </li> 
+
+            <li class="nav-item dropdown">
+              <a href="#dollar-sign" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
+                <i class="fe fe-dollar-sign fe-16"></i>
+                <span class="ml-3 item-text">Finance</span>
+              </a>
+              <ul class="collapse list-unstyled pl-4 w-100" id="dollar-sign">
+                <a class="nav-link pl-3" href="income.php"><span class="ml-1">Income</span></a>
+                <a class="nav-link pl-3" href="expense.php"><span class="ml-1">Expense</span></a>
+              </ul>
+            </li>
+
+            <li class="nav-item dropdown">
+              <a href="#clipboard" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
+                <i class="fe fe-clipboard fe-16"></i>
+                <span class="ml-3 item-text">Report</span>
+              </a>
+              <ul class="collapse list-unstyled pl-4 w-100" id="clipboard">
+                <a class="nav-link pl-3" href="income-report.php"><span class="ml-1">Income Report</span></a>
+                <a class="nav-link pl-3" href="expense-report.php"><span class="ml-1">Expense Report</span></a>
+              </ul>
+            </li>
+
+          </ul>
+        
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </main> <!-- main -->
+    </div> <!-- .wrapper -->
