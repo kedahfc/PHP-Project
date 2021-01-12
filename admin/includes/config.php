3ba6e725f23c8ac2929287c559c8cddbc7e95343
kedahfc <77327984+kedahfc@users.noreply.github.com>
2021-01-13 03:04:19 +0800

    Update config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,13 +1,16 @@
-<?php 
-// DB credentials.
-define('DB_HOST','us-cdbr-east-03.cleardb.com');
-define('DB_USER','b6be6324539b6d');
-define('DB_PASS','57f7729');
-define('DB_NAME','heroku_fabc276d92d4c52');
-// Establish database connection.
+<?php
+
+//Get Heroku ClearDB connection information
+$cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server   = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db       = substr($cleardb_url["path"],1);
+
 $active_group = 'default';
 $query_builder = TRUE;
 
 // Connect to DB
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
 ?>
