778798a2efe794129fe2c15ade772822c2f48548
kedahfc <77327984+kedahfc@users.noreply.github.com>
2021-01-12 21:04:01 +0800

    Add files via upload

---
+++ user/includes/footer.php
@@ -0,0 +1,121 @@
+<!-- ======= Footer ======= -->
+<footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6">
+            <div class="footer-info">
+              <h3>Contact Details</h3>
+
+              <p>  
+                <img src="assets/img/icon/location.png" />Kedah Football Club,<br>
+                <img src="assets/img/icon/green.png" />Level 1, Main Grandstand,<br>
+                <img src="assets/img/icon/green.png" />Darul Aman Stadium,<br>
+                <img src="assets/img/icon/green.png" />05100 Alor Setar, Kedah<br>
+                <img src="assets/img/icon/phone.png" />04-733 9960<br>
+                <img src="assets/img/icon/email.png" />enquiries@kedahfc.com.my<br>
+                <img src="assets/img/icon/schedule.png" />Sunday to Thursday 9AM - 5PM<br>
+              </p>
+
+              <div class="social-links mt-3">
+                <img src="assets/img/icon/green.png" />
+                <a href="https://www.facebook.com/kdafc.my/" class="facebook"><i class="bx bxl-facebook"></i></a>
+                <a href="https://www.instagram.com/official_kedahfa/" class="instagram"><i class="bx bxl-instagram"></i></a>
+                <a href="https://twitter.com/official_kfa" class="twitter"><i class="bx bxl-twitter"></i></a>
+                <a href="https://www.youtube.com/c/KedahFA" class="youtube"><i class="bx bxl-youtube"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6">
+              <div class="footer-info">
+                <h3>Location</h3>
+                 <iframe style="border:0; width: 100%; height: 270px;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15867.847945183188!2d100.371809!3d6.13581!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc23d6283d4630f63!2sStadium%20Darul%20Aman!5e0!3m2!1sen!2smy!4v1608607829573!5m2!1sen!2smy" frameborder="0" allowfullscreen>
+                </iframe>
+            </div>
+          </div>
+
+            <div class="col-lg-3 col-md-6">
+              <div class="footer-info">
+                <h3>About Us</h3>
+                Kedah Football Club is a football club that based in Alor Setar, Kedah, Malaysia, that competes in the Malaysia Super League.
+                They are the only team in the history of Malaysian football to have achieved treble titles twice, in 2006–07 and 2007–08 seasons.
+                This club represents the state of Kedah, under the supervision of Kedah Football Association. 
+                Kedah FA currently plays in the Malaysia Super League.
+              </div>
+            </div>
+ 
+            <div class="col-lg-3 col-md-6 footer-links">
+            <div class="footer-info">
+            <h3>Useful Links</h3>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="index.php">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="about-us.php">About us</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="news.php">News</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="current-squad.php">Current Squad</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="results.php">Results</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="membership.php">Membership</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="shop.php">shop</a></li>
+
+            </ul>
+          </div>
+
+      </div>
+    </div>
+  </div>
+
+    <!-- ======= Clients Section ======= -->
+<section id="clients" class="clients section-bg">
+  <div class="container">
+
+    <div class="row">
+
+      <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+        <img src="assets/img/sponsor/1 chenang-bay.png" class="img-fluid" alt="">
+      </div>
+
+      <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+        <img src="assets/img/sponsor/2 mbki.png" class="img-fluid" alt="">
+      </div>
+
+      <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+        <img src="assets/img/sponsor/3 PKNK.png" class="img-fluid" alt="">
+      </div>
+
+      <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+        <img src="assets/img/sponsor/4 LOTTO.png" class="img-fluid" alt="">
+      </div>
+
+      <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+        <img src="assets/img/sponsor/5 cosmic.jpg" class="img-fluid" alt="">
+      </div>
+
+      <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+        <img src="assets/img/sponsor/6 MyEG.png" class="img-fluid" alt="">
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</div>
+
+
+</section><!-- End Clients Section -->
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright 2020<strong><span> | Kedah Football Club</span></strong> | All Rights Reserved
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+
+
+
+
+
+  
---
+++ user/includes/header.php
@@ -0,0 +1,77 @@
+ <!-- ======= Header ======= -->
+ <header id="header" class="fixed-top" style="margin-top:-10px">
+    <div class="container d-flex align-items-center">
+
+      <nav class="nav-menu d-none d-lg-block">
+
+        <ul>
+
+        <li class="drop-down" style=margin-left:-10px><a href="#">About</a>
+            <ul>
+              <li><a href="page.php?type=aboutus">About Us</a></li>
+              <li><a href="page.php?type=history">History</a></li>
+              <li class="drop-down"><a href="#">Club Structure</a>
+                <ul>
+                  <li><a href="senior-officials.php">Senior Officials</a></li>
+                  <li><a href="team-officials.php">Team Officials</a></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+
+          <li><a href="news.php">News</a></li>
+
+          <li class="drop-down"><a href="#">Teams</a>
+            <ul>
+              <li><a href="current-squad.php">Current Squad</a></li>
+              <li><a href="results.php">Results</a></li>
+            </ul>
+          </li>
+
+          <li class="drop-down"><a href="#">Standings</a>
+            <ul>
+              <li><a href="standings.php">Standings</a></li>
+              <li><a href="honours.php">Honours</a></li>
+            </ul>
+          </li>
+
+          <li><a href="../fixtures.php" style=margin-right:-30px>Fixtures</a></li>
+
+        </ul> 
+      </nav><!-- .nav-menu -->
+
+      <h1 class="logo" style=margin-top:1px><a href="index.php"></a>
+      <a href="index.php" class="logo"><img src="assets/img/logo/logo center 2.png" alt="" class="img-fluid"></a>
+      </h1>
+
+
+
+        <a href="agm.php" class="get-started-btn" style=margin-left:-140px >AGM</a>
+        <a href="ticket.php" class="get-started-btn" style=margin-left:-5px>Ticket</a>
+        <a href="membership.php" class="get-started-btn" style=margin-left:-5px>Membership</a>
+        <a href="shop.php" class="get-started-btn" style=margin-left:-5px>Shop</a>
+  
+        <nav class="nav-menu2 d-none d-lg-block">
+        <ul>
+          <li class="get-started-btn drop-down2" a href="" style=margin-left:-5px ><img src="assets/img/profile.png" alt="" class="img-fluid"></a>
+            <ul>
+              <li><a href="my-profile.php">My Profile</a></li>
+              <li><a href="view-membership.php">My Membership</a></li>
+              <li><a href="change-password.php">Change Password</a></li>
+              <li><a href="logout.php">Logout</a></li>
+            </ul>
+          </li>
+        </ul> 
+      </nav>
+
+    </div>
+
+    <div class=line></div>
+
+  </header>
+  
+  <!-- End Header -->
+
+ <!-- Template Main JS File -->
+ <script src="assets/js/main.js"></script>
+
